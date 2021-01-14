d3a9173033f885dfcdc256b2e26b70e139fa6161
David Engblom <kirellen1573@zoho.eu>
2021-01-14 16:58:39 +0100

    plz no break

--- Project/Assets/Plugins/Transitions/TransitionManager.cs
+++ Project/Assets/Plugins/Transitions/TransitionManager.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿﻿﻿using System;
 using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
@@ -7,55 +7,27 @@ namespace Plugins.Transition.Scripts
 {
     public class TransitionManager : MonoBehaviour//, ITransitionManager
     {
-        
-        // make a transition using a shader effect on the image
-        // render screenshot onto a 2d texture
-        // apply the rendered texture onto the canvas image
-        // make a transition using a shader effect on the image (e.g. pixelate)   
         // refactor / abstract stuff in this class.
         // custom options such as
         // pause game
         // image stays
         // transition to another level
         
-        
         private GameObject transitionScreen;
         private Image image;
-        private Sprite sprite;
+
+        public Shader transition;
+        private Material mat;
         
         private static readonly int Intensity = Shader.PropertyToID("_intensity");
+        private static readonly int Texture = Shader.PropertyToID("_texture");
 
-        [ContextMenu("FadeFromBlack")]
-        public void FadeFromBlack()
-        {
-            CreateFaderObject();
-            StartCoroutine(FadeFromBlack(1f));
-        }
-        
-        [ContextMenu("FadeToBlack")]
-        public void FadeToBlack()
-        {
-            CreateFaderObject();
-            StartCoroutine(FadeToBlack(1f));
-        }
-
-        [ContextMenu("Take Screenshot")]
-        public void TakeScreenShot()
+        [ContextMenu("Start Transition")]
+        public void StartTransition()
         {
             StartCoroutine(ScreenCaptureToTexture());
         }
 
-        [ContextMenu("PixelateIn")]
-        public void PixelateIn()
-        {
-            StartCoroutine(PixelateTransition());
-        }
-        [ContextMenu("PixelateOut")]
-        public void PixelateOut()
-        {
-            StartCoroutine(PixelateTransition(false));
-        }
-        
         private void CreateFaderObject()
         {
             transitionScreen = new GameObject();
@@ -64,78 +36,107 @@ namespace Plugins.Transition.Scripts
             image = tempCanvas.gameObject.AddComponent<Image>();
         }
         
-        private IEnumerator FadeFromBlack(float Duration)
-        {
-            Color color = Color.black;
-            image.material.color = color;
-            color.a = 1f;
-            while (image.material.color.a > 0)
-            {
-                color.a -= Time.deltaTime / Duration;
-                image.material.color = color;
-                yield return null;
-            }
-            color.a = 0f;
-            CleanUp();
-        }
-        
-        private IEnumerator FadeToBlack(float Duration)
-        {
-            Color color = Color.black;
-            color.a = 0f;
-            image.material.color = color;
-            while (image.material.color.a < 1f)
-            {
-                color.a += Time.deltaTime / Duration;
-                image.material.color = color;
-                yield return null;
-            }
-            color.a = 1f;
-            CleanUp();
-        }
-        
         IEnumerator ScreenCaptureToTexture()
         {
+            CreateFaderObject();
+            this.image.material = this.mat;
+            this.image.material.shader = this.transition;
+            this.image.material.SetFloat(Intensity, 0); 
+
             yield return new WaitForEndOfFrame();
-            
-            CreateFaderObject();
             var texture = ScreenCapture.CaptureScreenshotAsTexture();
-            
-            //todo
-            
+
+            this.image.material.SetTexture(Texture, texture);
+
             Debug.Log("Screen captured");
-            
-            //Destroy(texture);
+
+            StartCoroutine(DoTransition(true, 0.5f));
         }
-        private IEnumerator PixelateTransition(bool pixelate = true, float speed = 2)
+        
+        private IEnumerator DoTransition(bool direction = true, float speed = 2)
         {
-            CreateFaderObject();
-            ApplyTexture(image.material);
-            var pixelateValue = this.image.material.GetFloat(Intensity);
-            if (pixelate)
+            var value = this.image.material.GetFloat(Intensity);
+            if (direction)
             {
-                while (pixelateValue < 1)
+                while (value < 1)
                 {
-                    this.image.material.SetFloat(Intensity, pixelateValue += speed * Time.deltaTime);
+                    this.image.material.SetFloat(Intensity, value += speed * Time.deltaTime);
                     yield return null;
                 }
             }
             else
             {
-                while (pixelateValue > 0)
+                while (value > 0)
                 {
-                    this.image.material.SetFloat(Intensity, pixelateValue -= speed * Time.deltaTime);
+                    this.image.material.SetFloat(Intensity, value -= speed * Time.deltaTime);
                     yield return null;
                 }
             }
+            Destroy(this.transitionScreen);
         }
+        
+        // [ContextMenu("FadeFromBlack")]
+        // public void FadeFromBlack()
+        // {
+        //     CreateFaderObject();
+        //     StartCoroutine(FadeFromBlack(1f));
+        // }
+        //
+        // [ContextMenu("FadeToBlack")]
+        // public void FadeToBlack()
+        // {
+        //     CreateFaderObject();
+        //     StartCoroutine(FadeToBlack(1f));
+        // }
+        //
+        //
+        // [ContextMenu("PixelateIn")]
+        // public void PixelateIn()
+        // {
+        //     StartCoroutine(PixelateTransition());
+        // }
+        // [ContextMenu("PixelateOut")]
+        // public void PixelateOut()
+        // {
+        //     StartCoroutine(PixelateTransition(false));
+        // }
 
-        private void ApplyTexture(Material material)
-        {
-            //todo
-            material.SetTexture("_texture", image.material.mainTexture);
-        }
+        // private void ApplyTexture(Material material)
+        // {
+        //     //todo
+        //     material.SetTexture("_texture", image.material.mainTexture);
+        // }
 
-        private void CleanUp() => Destroy(transitionScreen);
+        // private void CleanUp() => Destroy(transitionScreen);
+        
+        // private IEnumerator FadeToBlack(float Duration)
+        // {
+        //     Color color = Color.black;
+        //     color.a = 0f;
+        //     image.material.color = color;
+        //     while (image.material.color.a < 1f)
+        //     {
+        //         color.a += Time.deltaTime / Duration;
+        //         image.material.color = color;
+        //         yield return null;
+        //     }
+        //     color.a = 1f;
+        //     CleanUp();
+        // }
+        
+        // private IEnumerator FadeFromBlack(float Duration)
+        // {
+        //     Color color = Color.black;
+        //     image.material.color = color;
+        //     color.a = 1f;
+        //     while (image.material.color.a > 0)
+        //     {
+        //         color.a -= Time.deltaTime / Duration;
+        //         image.material.color = color;
+        //         yield return null;
+        //     }
+        //     color.a = 0f;
+        //     CleanUp();
+        // }
     }
 }
