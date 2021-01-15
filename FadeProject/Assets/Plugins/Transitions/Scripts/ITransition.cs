d2d4d058f0042a5f05b9c532b01af31ffbe62ac1
dafors <da_fors@hotmail.com>
2021-01-15 10:10:24 +0100

    add 2019 custom render pass project

---
+++ FadeProject/Assets/Plugins/Transitions/Scripts/ITransition.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Plugins.Transitions.Scripts
+{
+    public interface ITransition
+    {
+        IEnumerator Forward(Material material, float duration);
+        IEnumerator Reverse(Material material, float duration);
+    }
+}
